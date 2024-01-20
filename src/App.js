d781650a4d377b79a65c77f1a76510007ceb205d
hardNormalEasyMode <petequdwns12@naver.com>
2024-01-20 15:53:22 +0900

    App.js import page
    app.js import page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 
-import HomePage from "./views/page/Home/HomePage";
+//페이지 연결
+import InvitePage from "./views/page/Invitation/InvitePage/InvitePage";
+import Ininvitation1 from "./views/page/Invitation/Ininvitation1/Ininvitation1";
+import Ininvitation2 from "./views/page/Invitation/Ininvitation2/Ininvitation2";
+import Ininvitation3 from "./views/page/Invitation/Ininvitation3/Ininvitation3";
+import Ininvitation4 from "./views/page/Invitation/Ininvitation4/Ininvitation4";
+import Ininvitation5 from "./views/page/Invitation/Ininvitation5/Ininvitation5";
+import LastPage from "./views/page/Invitation/LastPage/LastPage";
 
 function App() {
 	return (
