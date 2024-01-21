c9821925dbb4f7072ba0bb47353213a7879a5a37
hardNormalEasyMode <petequdwns12@naver.com>
2024-01-21 13:38:11 +0900

    InvitePage.css add
    InvitePage.css add

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 
 //페이지 연결
+import InvitePage from "./views/page/Invitation/InvitePage/InvitePage";
 import LastPage from "./views/page/Invitation/LastPage/LastPage";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
 		<Suspense fallback={<div>Loading...</div>}>
 			<div>
 				<Routes>
+					<Route exact path="/" element={<InvitePage />} />
 					<Route exact path="/" element={<LastPage />} />
 				</Routes>
 			</div>
