f7023d2a3359281559cadab95dfb0a4c30bb38ae
Kim Seongjin <darkmoon625@naver.com>
2024-01-22 16:48:49 +0900

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 
-import HomePage from "./views/page/Home/HomePage";
+import About from "./views/page/About/About";
+import Header from "./views/page/About/components/Header";
 
 function App() {
 	return (
 		<Suspense fallback={<div>Loading...</div>}>
-			<div>
-				<Routes>
-					<Route exact path="/" element={<HomePage />} />
-				</Routes>
-			</div>
+			<Header />
+			<Routes>
+				<Route exact path="/" element={<About />} />
+			</Routes>
 		</Suspense>
 	);
 }
---
+++ src/views/page/About/components/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "../../../../styles/About/components/Header.css";
+
+function Header() {
+	function handleAboutClick() {
+		window.location.href = "/";
+	}
+
+	function handleProjectsClick() {
+		window.location.href = "/";
+	}
+
+	function handleRecordTapeClick() {
+		window.location.href = "/";
+	}
+
+	return (
+		<div className="container">
+			<div className="logo-left">in a row</div>
+			<div className="nav-element">
+				<div onClick={handleAboutClick}>About</div>
+				<div onClick={handleProjectsClick}>Projects</div>
+				<div onClick={handleRecordTapeClick}>Record Tape</div>
+			</div>
+			<div className="logo-right">CLOVER</div>
+		</div>
+	);
+}
+
+export default Header;
