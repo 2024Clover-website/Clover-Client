a5b1a26241f2aee275dde5ed056b6fc29bb7715a
SangHun Kwon <jun00739@naver.com>
2024-01-24 16:04:30 +0900

    responisve
    PC, Tablet, mobile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 
 import About from "./views/page/About/About";
 import Header from "./views/components/Header";
 import Footer from "./views/components/Footer";
 
 function App() {
+
+	const isPc = useMediaQuery({
+		query : "(min-width:1195px)"
+	});
+	const isTablet = useMediaQuery({
+		query : "(min-width:376px) and (max-width:1194px)"
+	});
+	const isMobile = useMediaQuery({
+		query : "(max-width:375px)"
+	});
+
+
 	return (
 		<Suspense fallback={<div>Loading...</div>}>
 			<div>
