a0173eb2b31b2785830859dac3cdbfb2f621c08a
Kim Seongjin <darkmoon625@naver.com>
2024-02-02 01:58:15 +0900

    Complete Footer Tab

--- src/views/components/Footer.js
+++ src/views/components/Footer.js
@@ -3,9 +3,19 @@ import React from "react";
 import "../../styles/components/Footer.css";
 
 function Footer() {
+	function handleInstagramClick() {
+		window.location.replace("https://www.instagram.com");
+	}
+
 	return (
 		<div className="container">
-			<div>Footer Area</div>
+			<div className="team">
+				MJU Division of Design CLOVER Final Exhibition 2024
+			</div>
+			<div className="location">Maru Art Center 3F The Second Gallery</div>
+			<div className="instagram" onClick={handleInstagramClick}>
+				Instagram
+			</div>
 		</div>
 	);
 }
