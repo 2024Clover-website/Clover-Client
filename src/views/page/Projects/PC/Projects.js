f328578f07d38e08f540d083cda031f871304986
youminki <108206432+youminki@users.noreply.github.com>
2024-02-14 20:32:47 +0900

    version1

--- src/views/page/Projects/PC/Projects.js
+++ src/views/page/Projects/PC/Projects.js
@@ -1,7 +1,6 @@
 import React from "react";
-
-import styles from "../../../../styles/Projects/PC/Projects.module.css";
 import { IoPlayCircle } from "react-icons/io5";
+import styles from "../../../../styles/Projects/PC/Projects.module.css";
 
 function Projects() {
 	function handleDocentButton() {
@@ -14,8 +13,53 @@ function Projects() {
 
 	return (
 		<div className={styles.container}>
-			<div className={styles.imageContainer}>
-				<img alt src="./logo512.png" />
+			<div className={styles.CTMain}>
+				<img alt src="/img/CTMain.png" />
+				<div className={styles.CTA}>
+					<img alt src="/img/CTA.png" />
+					<div className={styles.alphabetA}>
+						<img alt src="/img/alphabetA.png" />
+					</div>
+				</div>
+				<div className={styles.CTB}>
+					<img alt src="/img/CTB.png" />
+					<div className={styles.alphabetB}>
+						<img alt src="/img/alphabetB.png" />
+					</div>
+				</div>
+				<div className={styles.CTC}>
+					<img alt src="/img/CTC.png" />
+					<div className={styles.alphabetC}>
+						<img alt src="/img/alphabetC.png" />
+					</div>
+				</div>
+				<div className={styles.CTD}>
+					<img alt src="/img/CTD.png" />
+					<div className={styles.alphabetD}>
+						<img alt src="/img/alphabetD.png" />
+					</div>
+				</div>
+				<div className={styles.CTE}>
+					<img alt src="/img/CTE.png" />
+					<div className={styles.alphabetE}>
+						<img alt src="/img/alphabetE.png" />
+					</div>
+				</div>
+				<div className={styles.CTF}>
+					<img alt src="/img/CTF.png" />
+					<div className={styles.alphabetF}>
+						<img alt src="/img/alphabetF.png" />
+					</div>
+				</div>
+				<div className={styles.CT01}>
+					<img alt src="/img/CT01.png" />
+				</div>
+				<div className={styles.CT02}>
+					<img alt src="/img/CT02.png" />
+				</div>
+				<div className={styles.CT03}>
+					<img alt src="/img/CT03.png" />
+				</div>
 			</div>
 			<div className={styles.descriptionContainer}>
 				<div className={styles.titleBox}>
