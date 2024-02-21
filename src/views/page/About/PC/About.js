ea7340536c086ae719a2ccd8034e6fea018ef458
youminki <108206432+youminki@users.noreply.github.com>
2024-02-21 17:34:20 +0900

    about

--- src/views/page/About/PC/About.js
+++ src/views/page/About/PC/About.js
@@ -1,11 +1,22 @@
 import React from "react";
+import styles from '../../../../styles/About/PC/About.modul.css';
 
 function About() {
-	return (
-		<div style={{ paddingTop: 100 }}>
-			<div> TEST PC</div>
-		</div>
-	);
+    return (
+        <div className={styles.container}>
+            {/* <div className={styles.page1}>
+                <img src="./img/casetteTape.png"></img>
+            </div> */}
+            
+            <div className={styles.page2}>
+                <div className={styles.Linecontainer}>s</div>
+                
+            </div>
+        </div>
+    );
 }
 
 export default About;
+
+
+
