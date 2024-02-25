5cc74863ee89377a17e77eea6812d81568cfa8f9
hardNormalEasyMode <petequdwns12@naver.com>
2024-02-25 21:11:17 +0900

    pencil img add

--- src/views/page/REC/REC.js
+++ src/views/page/REC/REC.js
@@ -15,9 +15,9 @@ function REC(){
             <div className={styles.Start}>나만의 연필을 만들어볼까요?</div>
             <div className={styles.pencilContainer}>
                 <img className={styles.pencil} src={pencilimg} alt="loding Image"></img>
-            </div>
-            <div className={styles.ButtonContainer}>
-                <button onClick={handleRE2Cclick} className={styles.stButton}>시작하기</button>
+                <div className={styles.ButtonContainer}>
+                    <button onClick={handleRE2Cclick} className={styles.stButton}>시작하기</button>
+                </div>
             </div>
         </div>
     );
