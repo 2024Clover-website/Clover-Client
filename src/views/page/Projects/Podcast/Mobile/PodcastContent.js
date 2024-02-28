53b89ed5e63b7bbb16f83f876c7f29f9e54ac1c9
Kim Seongjin <darkmoon625@naver.com>
2024-02-29 06:30:23 +0900

    for test

--- src/views/page/Projects/Podcast/Mobile/PodcastContent.js
+++ src/views/page/Projects/Podcast/Mobile/PodcastContent.js
@@ -7,7 +7,7 @@ import { useNavigate, useLocation } from "react-router-dom";
 import styles from "../../../../../styles/Projects/Docent/Mobile/DocentContent.module.css";
 
 function PodcastContent() {
-	const navigate = useNavigate();
+	// const navigate = useNavigate();
 	const location = useLocation();
 
 	const audioRef = useRef();
@@ -15,30 +15,30 @@ function PodcastContent() {
 	const record = location.state.record;
 	const teamId = location.state.teamId;
 	const background = location.state.background;
-	const commentCount = location.state.commentCount;
+	// const commentCount = location.state.commentCount;
 
 	const [progress, setProgress] = useState(100);
 	const [playbackRate, setPlaybackRate] = useState(1);
-	const [isMuted, setIsMuted] = useState(false);
+	// const [isMuted, setIsMuted] = useState(false);
 	const [script, setScript] = useState([]);
 	const [runningTime, setRunningTime] = useState(0);
 	const [isLoading, setIsLoading] = useState(true);
 
 	let relativePosition;
 
-	const handleSpeedClick = () => {
-		if (audioRef.current) {
-			if (!audioRef.current.paused) {
-				if (playbackRate !== 2) {
-					setPlaybackRate(playbackRate * 2);
-					audioRef.current.playbackRate *= 2;
-				} else {
-					setPlaybackRate(0.5);
-					audioRef.current.playbackRate = 0.5;
-				}
-			}
-		}
-	};
+	// const handleSpeedClick = () => {
+	// 	if (audioRef.current) {
+	// 		if (!audioRef.current.paused) {
+	// 			if (playbackRate !== 2) {
+	// 				setPlaybackRate(playbackRate * 2);
+	// 				audioRef.current.playbackRate *= 2;
+	// 			} else {
+	// 				setPlaybackRate(0.5);
+	// 				audioRef.current.playbackRate = 0.5;
+	// 			}
+	// 		}
+	// 	}
+	// };
 
 	useEffect(() => {
 		async function fetchData() {
@@ -90,77 +90,77 @@ function PodcastContent() {
 		}
 	}, [progress, runningTime, playbackRate, isLoading]);
 
-	const handleProgressBar = (event) => {
-		if (!isLoading) {
-			// 클릭한 위치의 x 좌표 구하기
-			const clickX = event.clientX;
+	// const handleProgressBar = (event) => {
+	// 	if (!isLoading) {
+	// 		// 클릭한 위치의 x 좌표 구하기
+	// 		const clickX = event.clientX;
 
-			// div 요소 가져오기
-			const div = document.getElementById("play");
+	// 		// div 요소 가져오기
+	// 		const div = document.getElementById("play");
 
-			// div의 위치 및 너비 구하기
-			const divRect = div.getBoundingClientRect();
-			const divX = divRect.left;
-			const divWidth = divRect.width;
+	// 		// div의 위치 및 너비 구하기
+	// 		const divRect = div.getBoundingClientRect();
+	// 		const divX = divRect.left;
+	// 		const divWidth = divRect.width;
 
-			// 클릭한 위치의 div 내부에서의 상대적인 x 좌표 구하기
-			const relativeX = clickX - divX;
+	// 		// 클릭한 위치의 div 내부에서의 상대적인 x 좌표 구하기
+	// 		const relativeX = clickX - divX;
 
-			// 클릭한 위치의 div 내부에서의 상대적인 위치 (0 ~ 1) 구하기
-			relativePosition = (relativeX / divWidth).toFixed(3);
+	// 		// 클릭한 위치의 div 내부에서의 상대적인 위치 (0 ~ 1) 구하기
+	// 		relativePosition = (relativeX / divWidth).toFixed(3);
 
-			// 상대적인 위치 출력
-			console.log("Relative position:", relativePosition);
+	// 		// 상대적인 위치 출력
+	// 		console.log("Relative position:", relativePosition);
 
-			audioRef.current.play();
-			audioRef.current.currentTime = relativePosition * runningTime;
-			setProgress(100 - relativePosition * 100);
-		}
-	};
+	// 		audioRef.current.play();
+	// 		audioRef.current.currentTime = relativePosition * runningTime;
+	// 		setProgress(100 - relativePosition * 100);
+	// 	}
+	// };
 
-	const profileList = (script) =>
-		script.profile.map((profile, index) => {
-			return <img alt="" src={profile} />;
-		});
+	// const profileList = (script) =>
+	// 	script.profile.map((profile, index) => {
+	// 		return <img alt="" src={profile} />;
+	// 	});
 
-	const scriptCard = script.map((script, index) => {
-		// Use a ternary operator to ensure a value is returned
-		return isLoading ? (
-			<div key={index}>Loading...</div> // Placeholder while loading
-		) : (
-			<>
-				<>
-					<div className={styles.avatar}>{profileList(script)}</div>
+	// const scriptCard = script.map((script, index) => {
+	// 	// Use a ternary operator to ensure a value is returned
+	// 	return isLoading ? (
+	// 		<div key={index}>Loading...</div> // Placeholder while loading
+	// 	) : (
+	// 		<>
+	// 			<>
+	// 				<div className={styles.avatar}>{profileList(script)}</div>
 
-					<p
-						style={
-							audioRef.current &&
-							audioRef.current.currentTime <= script.end_time &&
-							audioRef.current.currentTime >= script.start_time
-								? {
-										opacity: 1,
-										transition: "opacity 0.5s cubic-bezier(0.42, 0, 0.58, 1)",
-								  }
-								: {
-										opacity: 0.4,
-										transition: "opacity 0.3s cubic-bezier(0.42, 0, 0.58, 1)",
-								  }
-						}
-						onClick={() => {
-							if (audioRef.current) {
-								audioRef.current.play();
-								audioRef.current.currentTime = script.start_time;
-								setProgress(100 - (script.start_time * 100) / runningTime);
-							}
-						}}
-					>
-						{script.script}
-					</p>
-					<br />
-				</>
-			</>
-		);
-	});
+	// 				<p
+	// 					style={
+	// 						audioRef.current &&
+	// 						audioRef.current.currentTime <= script.end_time &&
+	// 						audioRef.current.currentTime >= script.start_time
+	// 							? {
+	// 									opacity: 1,
+	// 									transition: "opacity 0.5s cubic-bezier(0.42, 0, 0.58, 1)",
+	// 							  }
+	// 							: {
+	// 									opacity: 0.4,
+	// 									transition: "opacity 0.3s cubic-bezier(0.42, 0, 0.58, 1)",
+	// 							  }
+	// 					}
+	// 					onClick={() => {
+	// 						if (audioRef.current) {
+	// 							audioRef.current.play();
+	// 							audioRef.current.currentTime = script.start_time;
+	// 							setProgress(100 - (script.start_time * 100) / runningTime);
+	// 						}
+	// 					}}
+	// 				>
+	// 					{script.script}
+	// 				</p>
+	// 				<br />
+	// 			</>
+	// 		</>
+	// 	);
+	// });
 
 	if (isLoading) {
 		console.log("loading");
